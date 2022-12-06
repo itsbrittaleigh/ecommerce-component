d5ad66e03028206f532fd8169ac748203b755265
Britta Rodenbeck <cheers@brittarodenbeck.com>
2022-12-06 13:02:50 -0600

    :lipstick: abstracting panel styles

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -5,26 +5,28 @@ import './index.css';
 const Menu = () => {
 
   return (
-    <section className="menu__section">
-      <h2 className="menu__heading">To Go Menu</h2>
-      {menuItems.map((item) => (
-        <div className="menu__item" key={item.id}>
-          <img
-            alt={item.alt}
-            className="menu-item__image"
-            src={`./images/${item.image}`}
-          />
-          <h3 className="menu-item__name">{item.name}</h3>
-          <p className="menu-item__price">{`$${formatPrice(item.price)}`}</p>
-          <button
-            className="menu-item__button"
-            onClick={() => addToCart(item.id)}
-            type="button"
-          >
-            Add to cart
-          </button>
-        </div>
-      ))}
+    <section className="panel__section">
+      <h2 className="panel__heading">To Go Menu</h2>
+      <div className="panel__content">
+        {menuItems.map((item) => (
+          <div className="menu__item" key={item.id}>
+            <img
+              alt={item.alt}
+              className="menu-item__image"
+              src={`./images/${item.image}`}
+            />
+            <h3 className="menu-item__name">{item.name}</h3>
+            <p className="menu-item__price">{`$${formatPrice(item.price)}`}</p>
+            <button
+              className="menu-item__button"
+              onClick={() => addToCart(item.id)}
+              type="button"
+            >
+              Add to cart
+            </button>
+          </div>
+        ))}
+      </div>
     </section>
   );
 
